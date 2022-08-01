3eafd6eee636911a2d1bd3e9f1f2c2cddaef1e0e
Elsayed Elghazy <elsayedelghazy20@gmail.com>
2022-08-01 22:23:40 +0200

    third commit

--- install.sh
+++ install.sh
@@ -6,6 +6,12 @@ initial_check(){
 
 }
 
+download_mega_packages(){
+
+        wget https://github.com/MxExGxA/Mega-AutoVps/tree/master/mega-packages
+
+}
+
 install_iptables_persistent(){
 
         echo iptables-persistent iptables-persistent/autosave_v4 boolean true | debconf-set-selections
@@ -43,7 +49,7 @@ install_depends(){
 
 
 mega-install(){
-	cd -P /root/mega_script/mega-packages/
+	cd -P mega-packages/
 	chmod +x mega_script
 	chmod +x openvpn/openvpn.sh
 	chmod +x squid/squid.sh
@@ -51,6 +57,8 @@ mega-install(){
 	cd -P /root/mega_script/
 	cp -r mega-packages /etc/mega-packages
 	ln /etc/mega-packages/mega_script /bin/mega
+        cd -P ../
+        rm -r mega-packages/
 }
 
 
@@ -63,8 +71,9 @@ echo "root permission checked!"
 echo ""
 sleep 2
 update_vps
-install_depends lsof curl iptables
+install_depends lsof curl iptables wget
 install_iptables_persistent
+download_mega_packages
 mega-install
 mega
 else
